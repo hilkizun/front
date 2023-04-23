884c2f2ae8bc1ad458d698dbd1560e60cbf2fff4
hilkizun <hilkizun@gmail.com>
2023-04-23 17:43:11 +0200

    profile styles

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -37,6 +37,7 @@ const Profile = () => {
   const [purchaseProducts, setPurchaseProducts] = useState([]);
   const [filterCategory, setFilterCategory] = useState(null);
   const [filterType, setFilterType] = useState('all');
+  const [showSoldItems, setShowSoldItems] = useState(false);
 
   useEffect(() => {
     const fetchLikedItems = async () => {
@@ -49,11 +50,11 @@ const Profile = () => {
 
   useEffect(() => {
     const fetchUserItems = async () => {
-        const products = await getUserProducts();
-        const auctions = await getUserAuctions();
-        setUserProducts(products);
-        setUserAuctions(auctions);
-      };
+      const products = await getUserProducts();
+      const auctions = await getUserAuctions();
+      setUserProducts(products);
+      setUserAuctions(auctions);
+    };
 
     fetchUserItems();
   }, [currentUser]);
@@ -84,6 +85,10 @@ const Profile = () => {
     setFilterType((prevType) => (prevType === type ? 'all' : type));
   };
 
+  const handleHideSoldItems = () => {
+    setShowSoldItems(!showSoldItems);
+  };
+
   const applyFilters = (items) => {
     return items
       .filter((item) => !filterCategory || item.category === filterCategory)
@@ -91,9 +96,13 @@ const Profile = () => {
         if (filterType === 'all') return true;
         if (filterType === 'product') return !item.endDate;
         if (filterType === 'auction') return !!item.endDate;
+        if (filterType === 'liked') return filteredLikedItems.some(liked => liked._id === item._id);
+        if (filterType === 'user') return filteredUserProducts.some(userProduct => userProduct._id === item._id);
+        if (filterType === 'purchased') return filteredPurchaseProducts.some(purchasedProduct => purchasedProduct._id === item._id);
         return true;
       });
   };
+
 
   const filteredWonProducts = applyFilters(wonProducts);
   const filteredLikedItems = applyFilters(likedItems);
@@ -107,7 +116,7 @@ const Profile = () => {
       <h1>
         Hola, {currentUser.firstName}
       </h1>
-      
+
       <div className="filters">
         <div className="filter-type">
           <button
@@ -122,89 +131,100 @@ const Profile = () => {
           >
             Subastas
           </button>
-        </div>
-        <div className="filter-category">
-          {categoryEnum.map((category) => (
+          <button
+            onClick={() => handleTypeChange('liked')}
+            className={`type-button ${filterType === 'liked' ? 'selected' : ''}`}
+          >
             <button
-              key={category}
-              onClick={() => handleCategoryChange(category)}
-              className={`category-button ${filterCategory === category ? 'selected' : ''}`}
-            >
-              {categoryNames[category]}
-            </button>
-          ))}
+              onClick={handleHideSoldItems}
+              className={`type-button ${showSoldItems ? 'selected' : ''}`}
+            ></button>
+            Me gusta
+          </button>
+          <button
+            onClick={() => handleTypeChange('user')}
+            className={`type-button ${filterType === 'user' ? 'selected' : ''}`}
+          >
+            Mis artículos
+          </button>
+          <button
+            onClick={() => handleTypeChange('purchased')}
+            className={`type-button ${filterType === 'purchased' ? 'selected' : ''}`}
+          >
+            Comprados
+          </button>
         </div>
       </div>
       <div className="content">
-      {filteredWonProducts.length > 0 && (
-        <>
-          <h2>¡Enhorabuena has ganado esta Subasta!</h2>
-          <div className="won-items">
-            {filteredWonProducts.map((product) => (
-              <Card
-                key={`product-${product._id}`}
-                item={product}
-                type="product"
-              />
-            ))}
-          </div>
-        </>
-      )}
-
-      {filteredLikedItems.length > 0 && (
-        <>
-          <h2>Has dado like</h2>
-          <div className="liked-items">
-            {filteredLikedItems.map((like) => {
-              const item = like.auction || like.product;
-              return (
+        {filteredWonProducts.length > 0 && (
+          <>
+            <h2>¡Enhorabuena has ganado esta Subasta!</h2>
+            <div className="won-items">
+              {filteredWonProducts.map((product) => (
                 <Card
-                  key={`${item.endDate ? 'auction' : 'product'}-${item._id}`}
-                  item={item}
-                  type={item.endDate ? 'auction' : 'product'}
+                  key={`product-${product._id}`}
+                  item={product}
+                  type="product"
                 />
-              );
-            })}
-          </div>
-        </>
-      )}
+              ))}
+            </div>
+          </>
+        )}
 
-      {(filteredUserProducts.length > 0 || filteredUserAuctions.length > 0) && (
-        <>
-          <h2>Estos son tus artículos</h2>
-          <div className="user-items">
-            {filteredUserProducts.map((product) => (
-              <Card
-                key={`product-${product._id}`}
-                item={product}
-                type="product"
-              />
-            ))}
-            {filteredUserAuctions.map((auction) => (
-              <Card
-                key={`auction-${auction._id}`}
-                item={auction}
-                type="auction"
-              />
-            ))}
-          </div>
-        </>
-      )}
+        {filteredLikedItems.length > 0 && (
+          <>
+            <h2>Has dado like</h2>
+            <div className="liked-items">
+              {filteredLikedItems.map((like) => {
+                const item = like.auction || like.product;
+                return (
+                  <Card
+                    key={`${item.endDate ? 'auction' : 'product'}-${item._id}`}
+                    item={item}
+                    type={item.endDate ? 'auction' : 'product'}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
 
-      {filteredPurchaseProducts.length > 0 && (
-        <>
-          <h2>Productos comprados</h2>
-          <div className="purchase-items">
-            {filteredPurchaseProducts.map((product) => (
-              <Card
-                key={`product-${product._id}`}
-                item={product}
-                type="product"
-              />
-            ))}
-          </div>
-        </>
-      )}
+        {(filteredUserProducts.length > 0 || filteredUserAuctions.length > 0) && (
+          <>
+            <h2>Estos son tus artículos</h2>
+            <div className="user-items">
+              {filteredUserProducts.map((product) => (
+                <Card
+                  key={`product-${product._id}`}
+                  item={product}
+                  type="product"
+                />
+              ))}
+              {filteredUserAuctions.map((auction) => (
+                <Card
+                  key={`auction-${auction._id}`}
+                  item={auction}
+                  type="auction"
+                />
+              ))}
+            </div>
+          </>
+        )}
+
+        {filteredPurchaseProducts.length > 0 && (
+          <>
+            <h2>Productos comprados</h2>
+            <div className="purchase-items">
+              {filteredPurchaseProducts.map((product) => (
+                <Card
+                  key={`product-${product._id}`}
+                  item={product}
+                  type="product"
+                />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
